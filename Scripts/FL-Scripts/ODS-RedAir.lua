7a94cac4de654798795375813cd2c0dde18e75a0
Ricardo Maes <ricmzn@gmail.com>
2022-02-06 23:28:56 -0300

    Split CAP and GCI squadrons to avoid airborne unit counts getting mixed

--- Scripts/FL-Scripts/ODS-RedAir.lua
+++ Scripts/FL-Scripts/ODS-RedAir.lua
@@ -107,18 +107,25 @@ A2ADispatcher_Red:SetIntercept( 300 ) -- Calculated interception delay
 A2ADispatcher_Red:SetGciRadius( 150000 ) -- Intercept targets less than 150km away from airbases
 A2ADispatcher_Red:SetBorderZone( { CAPZone1, CAPZone2, CAPZone3, CAPZone4 } )
 
--- Aleppo CAP + intercept
+-- Aleppo CAP
 A2ADispatcher_Red:SetSquadron( "Aleppo", AIRBASE.Syria.Aleppo, { "M21", "M19", "M23" } )
 A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Aleppo" )
 A2ADispatcher_Red:SetSquadronLandingAtRunway( "Aleppo" )
 A2ADispatcher_Red:SetSquadronOverhead( "Aleppo", 1 )
 A2ADispatcher_Red:SetSquadronGrouping( "Aleppo", 2, true )
--- CAP zone and parameters: area, min alt, max alt, min speed patrol, max speed patrol, min speed engage, max speed engage)
 A2ADispatcher_Red:SetSquadronCap( "Aleppo", CAPZone1, 4000, 7000, 460, 900, 600, 1200 )
--- CAP interval parameters: max groups, min regen interval, max regen interval
 A2ADispatcher_Red:SetSquadronCapInterval( "Aleppo", 1, 350, 1400 )
--- GCI intercept parameters: min engage speed, max engage speed, max airborne aircraft, min regen interval, max regen interval
-A2ADispatcher_Red:SetSquadronGci( "Aleppo", 800, 1200, 4, 350, 1400 )
+-- CAP parameters: area, min alt, max alt, min speed patrol, max speed patrol, min speed engage, max speed engage)
+-- CAP interval parameters: max groups, min regen interval, max regen interval
+
+-- Aleppo GCI
+A2ADispatcher_Red:SetSquadron( "Aleppo GCI", AIRBASE.Syria.Aleppo, { "M21", "M19", "M23" } )
+A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Aleppo GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Aleppo GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Aleppo GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Aleppo GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Aleppo GCI", 800, 1200, 4, 350, 1400 )
+-- GCI parameters: min engage speed, max engage speed, max airborne aircraft, min regen interval, max regen interval
 
 -- Airspawn 1 CAP
 A2ADispatcher_Red:SetSquadron( "39th", "Airspawn 1", { "M21", "M19" } )
@@ -129,15 +136,15 @@ A2ADispatcher_Red:SetSquadronGrouping( "39th", 2, true )
 A2ADispatcher_Red:SetSquadronCap( "39th", CAPZone1, 4000, 7000, 500, 900, 600, 1200 )
 A2ADispatcher_Red:SetSquadronCapInterval( "39th", 0, 350, 1400 )
 
--- Kuweires intercept
-A2ADispatcher_Red:SetSquadron( "Kuweires", AIRBASE.Syria.Kuweires, { "M21", "M19" } )
-A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Kuweires" )
-A2ADispatcher_Red:SetSquadronLandingAtRunway( "Kuweires" )
-A2ADispatcher_Red:SetSquadronOverhead( "Kuweires", 1 )
-A2ADispatcher_Red:SetSquadronGrouping( "Kuweires", 2, true )
-A2ADispatcher_Red:SetSquadronGci( "Kuweires", 800, 1200, 4, 350, 1400 )
+-- Kuweires GCI
+A2ADispatcher_Red:SetSquadron( "Kuweires GCI", AIRBASE.Syria.Kuweires, { "M21", "M19" } )
+A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Kuweires GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Kuweires GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Kuweires GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Kuweires GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Kuweires GCI", 800, 1200, 4, 350, 1400 )
 
--- Bassel CAP + intercept
+-- Bassel CAP
 A2ADispatcher_Red:SetSquadron( "Bassel", AIRBASE.Syria.Bassel_Al_Assad, { "M21", "M23" } )
 A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Bassel" )
 A2ADispatcher_Red:SetSquadronLandingAtRunway( "Bassel" )
@@ -145,15 +152,22 @@ A2ADispatcher_Red:SetSquadronOverhead( "Bassel", 1 )
 A2ADispatcher_Red:SetSquadronGrouping( "Bassel", 2, true )
 A2ADispatcher_Red:SetSquadronCap( "Bassel", CAPZone2, 4000, 7000, 500, 900, 600, 1200 )
 A2ADispatcher_Red:SetSquadronCapInterval( "Bassel", 1, 350, 1400 )
-A2ADispatcher_Red:SetSquadronGci( "Bassel", 800, 1500, 6, 250, 700 )
+
+-- Bassel GCI
+A2ADispatcher_Red:SetSquadron( "Bassel GCI", AIRBASE.Syria.Bassel_Al_Assad, { "M21", "M23" } )
+A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Bassel GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Bassel GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Bassel GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Bassel GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Bassel GCI", 800, 1500, 6, 250, 700 )
 
 -- Hama intercept
-A2ADispatcher_Red:SetSquadron( "Hama", AIRBASE.Syria.Hama, { "M21" } )
-A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Hama" )
-A2ADispatcher_Red:SetSquadronLandingAtRunway( "Hama" )
-A2ADispatcher_Red:SetSquadronOverhead( "Hama", 1 )
-A2ADispatcher_Red:SetSquadronGrouping( "Hama", 2, true )
-A2ADispatcher_Red:SetSquadronGci( "Hama", 1000, 1500, 4, 250, 700 )
+A2ADispatcher_Red:SetSquadron( "Hama GCI", AIRBASE.Syria.Hama, { "M21" } )
+A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Hama GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Hama GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Hama GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Hama GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Hama GCI", 1000, 1500, 4, 250, 700 )
 
 -- Airspawn 2 CAP
 A2ADispatcher_Red:SetSquadron( "77th", "Airspawn 2", { "M21", "M23", "M25" } )
@@ -164,15 +178,22 @@ A2ADispatcher_Red:SetSquadronGrouping( "77th", 2, true )
 A2ADispatcher_Red:SetSquadronCap( "77th", CAPZone2, 4000, 7000, 500, 900, 900, 1500 )
 A2ADispatcher_Red:SetSquadronCapInterval( "77th", 0, 350, 1400 )
 
--- Palmyra CAP + intercept
-A2ADispatcher_Red:SetSquadron( "Palmyra", AIRBASE.Syria.Palmyra, { "M21", "M23", "M25" } )
+-- Palmyra CAP
+A2ADispatcher_Red:SetSquadron( "Palmyra", AIRBASE.Syria.Palmyra, { "M21", "M23" } )
 A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Palmyra" )
 A2ADispatcher_Red:SetSquadronLandingAtRunway( "Palmyra" )
 A2ADispatcher_Red:SetSquadronOverhead( "Palmyra", 1 )
 A2ADispatcher_Red:SetSquadronGrouping( "Palmyra", 2, true )
 A2ADispatcher_Red:SetSquadronCap( "Palmyra", CAPZone4, 7000, 10000, 500, 900, 900, 1800 )
 A2ADispatcher_Red:SetSquadronCapInterval( "Palmyra", 1, 350, 1400 )
-A2ADispatcher_Red:SetSquadronGci( "Palmyra", 1000, 1500, 4, 250, 700 )
+
+-- Palmyra GCI
+A2ADispatcher_Red:SetSquadron( "Palmyra GCI", AIRBASE.Syria.Palmyra, { "M21", "M23", "M25" } )
+A2ADispatcher_Red:SetSquadronTakeoffFromParkingHot( "Palmyra GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Palmyra GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Palmyra GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Palmyra GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Palmyra GCI", 1000, 1500, 4, 250, 700 )
 
 -- Airspawn 3 CAP
 A2ADispatcher_Red:SetSquadron( "38th", "Airspawn 3", { "M23", "M25", "M29" } )
@@ -183,22 +204,23 @@ A2ADispatcher_Red:SetSquadronGrouping( "38th", 2, true )
 A2ADispatcher_Red:SetSquadronCap( "38th", CAPZone3, 4000, 7000, 500, 900, 900, 1500 )
 A2ADispatcher_Red:SetSquadronCapInterval( "38th", 0, 350, 1400 )
 
--- Damascus intercept
-A2ADispatcher_Red:SetSquadron( "Damascus", AIRBASE.Syria.Mezzeh, { "M23", "M29" } )
+-- Damascus CAP
+A2ADispatcher_Red:SetSquadron( "Damascus", AIRBASE.Syria.Mezzeh, { "M29" } )
 A2ADispatcher_Red:SetSquadronTakeoffInAir( "Damascus" )
 A2ADispatcher_Red:SetSquadronLandingAtRunway( "Damascus" )
 A2ADispatcher_Red:SetSquadronOverhead( "Damascus", 1 )
 A2ADispatcher_Red:SetSquadronGrouping( "Damascus", 2, true )
-A2ADispatcher_Red:SetSquadronGci( "Damascus", 800, 1500, 6, 250, 700 )
+A2ADispatcher_Red:SetSquadronCap( "Damascus", CAPZone3, 4000, 7000, 500, 900, 900, 1500 )
+A2ADispatcher_Red:SetSquadronCapInterval( "Damascus", 1, 350, 900 )
 
--- Damascus Mig-29 CAP
-A2ADispatcher_Red:SetSquadron( "Damascus M29", AIRBASE.Syria.Mezzeh, { "M29" } )
-A2ADispatcher_Red:SetSquadronTakeoffInAir( "Damascus M29" )
-A2ADispatcher_Red:SetSquadronLandingAtRunway( "Damascus M29" )
-A2ADispatcher_Red:SetSquadronOverhead( "Damascus M29", 1 )
-A2ADispatcher_Red:SetSquadronGrouping( "Damascus M29", 2, true )
-A2ADispatcher_Red:SetSquadronCap( "Damascus M29", CAPZone3, 4000, 7000, 500, 900, 900, 1500 )
-A2ADispatcher_Red:SetSquadronCapInterval( "Damascus M29", 1, 350, 900 )
+-- Damascus GCI
+A2ADispatcher_Red:SetSquadron( "Damascus GCI", AIRBASE.Syria.Mezzeh, { "M23", "M29" } )
+A2ADispatcher_Red:SetSquadronTakeoffInAir( "Damascus GCI" )
+A2ADispatcher_Red:SetSquadronLandingAtRunway( "Damascus GCI" )
+A2ADispatcher_Red:SetSquadronOverhead( "Damascus GCI", 1 )
+A2ADispatcher_Red:SetSquadronGrouping( "Damascus GCI", 2, true )
+A2ADispatcher_Red:SetSquadronGci( "Damascus GCI", 800, 1500, 6, 250, 700 )
+
 
 -- Debug messages
 -- A2ADispatcher_Red:SetTacticalDisplay(true)
